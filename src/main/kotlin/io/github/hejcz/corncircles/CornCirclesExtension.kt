45b9f5152006a32238595accf96d65ff611da34b
hejcz <rubin94@gmail.com>
2020-02-16 20:49:25 +0100

    Add inns and cathedrals tiles when extension is applied

--- src/main/kotlin/io/github/hejcz/corncircles/CornCirclesExtension.kt
+++ src/main/kotlin/io/github/hejcz/corncircles/CornCirclesExtension.kt
@@ -1,6 +1,8 @@
 package io.github.hejcz.corncircles
 
+import io.github.hejcz.core.*
 import io.github.hejcz.setup.*
+import io.github.hejcz.util.commandValidator
 
 object CornCirclesExtension : Extension {
 
@@ -20,4 +22,90 @@ object CornCirclesExtension : Extension {
     override fun modify(deck: TilesSetup) {
         deck.addAndShuffle(Korn1, Korn2, Korn3, Korn4, Korn5, Korn6)
     }
+
+    private val AvoidCornCircleActionHandler = object : CommandHandler {
+        override fun isApplicableTo(command: Command): Boolean = command is AvoidCornCircleActionCmd
+
+        override fun beforeScoring(state: State, command: Command): GameChanges =
+            (command as AvoidCornCircleActionCmd).let {
+                GameChanges.noEvents(
+                    state.changeActivePlayer()
+                )
+            }
+    }
+
+    private val AvoidCornCircleActionValidator = commandValidator<AvoidCornCircleActionCmd> { state, _ ->
+        when (val tile = state.recentTile()) {
+            is CornCircleTile -> when {
+                state.currentPlayerPieces(tile.cornCircleEffect()).isNotEmpty() -> setOf(CantSkipPieceEvent)
+                else -> emptySet()
+            }
+            else -> emptySet()
+        }
+    }
+
+    private val AddPieceHandler = object : CommandHandler {
+        override fun isApplicableTo(command: Command): Boolean = command is AddPieceCmd
+
+        override fun beforeScoring(state: State, command: Command): GameChanges =
+            (command as AddPieceCmd).let {
+                GameChanges.noEvents(
+                    state.addPiece(command.position, command.piece, command.role)
+                        .changeActivePlayer()
+                )
+            }
+    }
+
+    private val AddPieceValidator = commandValidator<AddPieceCmd> { state, command ->
+        when (val tile = state.recentTile()) {
+            is CornCircleTile -> when {
+                !tile.cornCircleEffect().matches(command.role) -> setOf(InvalidPieceLocationEvent)
+                playerDoesNotHaveAnyPieceThere(state, command) -> setOf(InvalidPieceLocationEvent)
+                else -> emptySet()
+            }
+            else -> emptySet()
+        }
+    }
+
+    private val ChooseCornCircleActionHandler = object : CommandHandler {
+        override fun isApplicableTo(command: Command): Boolean = command is ChooseCornCircleActionCmd
+
+        override fun beforeScoring(state: State, command: Command): GameChanges =
+            (command as ChooseCornCircleActionCmd).let {
+                GameChanges.noEvents(
+                    state.changeActivePlayer()
+                )
+            }
+    }
+
+    private fun playerDoesNotHaveAnyPieceThere(state: State, command: AddPieceCmd) =
+        state.findPieces(command.position, command.role)
+            .none { state.currentPlayerId() == it.playerId }
+
+    private val RemovePieceHandler = object : CommandHandler {
+        override fun isApplicableTo(command: Command): Boolean = command is RemovePieceCmd
+
+        override fun beforeScoring(state: State, command: Command): GameChanges =
+            (command as RemovePieceCmd).let {
+                GameChanges.noEvents(
+                    state.removePiece(command.position, command.piece, command.role)
+                        .changeActivePlayer()
+                )
+            }
+    }
+
+    private val RemovePieceValidator = commandValidator<RemovePieceCmd> { state, command ->
+        when (val tile = state.recentTile()) {
+            is CornCircleTile -> when {
+                !tile.cornCircleEffect().matches(command.role) -> setOf(InvalidPieceLocationEvent)
+                playerDoesNotHaveSuchPieceThere(state, command) -> setOf(InvalidPieceLocationEvent)
+                else -> emptySet()
+            }
+            else -> emptySet()
+        }
+    }
+
+    private fun playerDoesNotHaveSuchPieceThere(state: State, command: RemovePieceCmd) =
+        state.findPieces(command.position, command.role)
+            .none { state.currentPlayerId() == it.playerId && it.pieceOnBoard.piece == command.piece }
 }
--- src/main/kotlin/io/github/hejcz/inn/InnAndCathedralsExtension.kt
+++ src/main/kotlin/io/github/hejcz/inn/InnAndCathedralsExtension.kt
@@ -2,15 +2,14 @@ package io.github.hejcz.inn
 
 import io.github.hejcz.core.*
 import io.github.hejcz.core.rule.*
+import io.github.hejcz.inn.tiles.*
 import io.github.hejcz.setup.*
 
 object InnAndCathedralsExtension : Extension {
-    override fun modify(rulesSetup: RulesSetup) {
-        rulesSetup.replace(RewardCompletedCastle::class, RewardCompletedCastle(castleScoring))
-        rulesSetup.replace(RewardIncompleteCastle::class, RewardIncompleteCastle(incompleteCastleScoring))
-        rulesSetup.replace(RewardCompletedRoad::class, RewardCompletedRoad(roadScoring))
-        rulesSetup.replace(RewardIncompleteRoad::class, RewardIncompleteRoad(roadScoring))
-    }
-
     override fun modify(piecesSetup: PiecesSetup) = piecesSetup.add(BigPiece)
+
+    override fun modify(deck: TilesSetup) {
+        deck.addAndShuffle(TileEA, TileEB, TileEC, TileED, TileEE, TileEF, TileEG, TileEH, TileEI, TileEJ,
+            TileEK, TileEL, TileEM, TileEN, TileEO, TileEP, TileEQ)
+    }
 }
